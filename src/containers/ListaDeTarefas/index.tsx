a45b726156390594a72bbec8ba6a7f20372dbe61
ViniKrieger <viniheiko@gmail.com>
2024-04-15 16:34:23 -0300

    Criando os cards e estilizando eles

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,3 +1,26 @@
-const ListaDeTarefas = () => <h2>Lista de tarefas</h2>
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
 
+const ListaDeTarefas = () => (
+  <Container>
+    <p>2 tarfeas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
+    <ul>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+    </ul>
+  </Container>
+)
 export default ListaDeTarefas
